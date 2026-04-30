cb20300c0957b456b9e1b8bc6d0b7d64f65a6086
haruto2002 <nkym20020422@gmail.com>
2026-04-30 07:39:41 +0000

    v5

--- debug.py
+++ debug.py
@@ -2,9 +2,12 @@ from ultralytics import YOLO
 
 
 def main():
-    model_sizes = ["x", "n", "s", "m", "l"]
-    for model_size in model_sizes:
-        model = YOLO(f"yolo26{model_size}-pose.pt")
+    # model_sizes = ["x", "n", "s", "m", "l"]
+    # for model_size in model_sizes:
+    #     model = YOLO(f"yolo26{model_size}-pose.pt")
+
+    model = YOLO("weights/yolo26/yolo26x.pt")
+    print(model.names)
 
 
 if __name__ == "__main__":
--- pipeline/detector/yolo26.py
+++ pipeline/detector/yolo26.py
@@ -4,6 +4,89 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 
+LABEL_MAP_ID_TO_NAME = {
+    0: "person",
+    1: "bicycle",
+    2: "car",
+    3: "motorcycle",
+    4: "airplane",
+    5: "bus",
+    6: "train",
+    7: "truck",
+    8: "boat",
+    9: "traffic light",
+    10: "fire hydrant",
+    11: "stop sign",
+    12: "parking meter",
+    13: "bench",
+    14: "bird",
+    15: "cat",
+    16: "dog",
+    17: "horse",
+    18: "sheep",
+    19: "cow",
+    20: "elephant",
+    21: "bear",
+    22: "zebra",
+    23: "giraffe",
+    24: "backpack",
+    25: "umbrella",
+    26: "handbag",
+    27: "tie",
+    28: "suitcase",
+    29: "frisbee",
+    30: "skis",
+    31: "snowboard",
+    32: "sports ball",
+    33: "kite",
+    34: "baseball bat",
+    35: "baseball glove",
+    36: "skateboard",
+    37: "surfboard",
+    38: "tennis racket",
+    39: "bottle",
+    40: "wine glass",
+    41: "cup",
+    42: "fork",
+    43: "knife",
+    44: "spoon",
+    45: "bowl",
+    46: "banana",
+    47: "apple",
+    48: "sandwich",
+    49: "orange",
+    50: "broccoli",
+    51: "carrot",
+    52: "hot dog",
+    53: "pizza",
+    54: "donut",
+    55: "cake",
+    56: "chair",
+    57: "couch",
+    58: "potted plant",
+    59: "bed",
+    60: "dining table",
+    61: "toilet",
+    62: "tv",
+    63: "laptop",
+    64: "mouse",
+    65: "remote",
+    66: "keyboard",
+    67: "cell phone",
+    68: "microwave",
+    69: "oven",
+    70: "toaster",
+    71: "sink",
+    72: "refrigerator",
+    73: "book",
+    74: "clock",
+    75: "vase",
+    76: "scissors",
+    77: "teddy bear",
+    78: "hair drier",
+    79: "toothbrush",
+}
+
 
 class YOLODetector:
     def __init__(
@@ -14,6 +97,7 @@ class YOLODetector:
         img_long_side: int | None,
         conf_threshold: float,
         iou_threshold: float,
+        classes: list[int] | None = None,
     ):
         assert model_size in ["n", "s", "m", "l", "x"], "Invalid model size"
         self.weight_path = Path(weight_dir) / f"yolo26{model_size}.pt"
@@ -21,7 +105,7 @@ class YOLODetector:
         self.img_long_side = img_long_side
         self.conf_threshold = conf_threshold
         self.iou_threshold = iou_threshold
-
+        self.classes = classes
         self.model = self.build_detector()
 
     def build_detector(self):
@@ -40,6 +124,7 @@ class YOLODetector:
             conf=self.conf_threshold,
             iou=self.iou_threshold,
             device=self.device,
+            classes=self.classes,
             verbose=False,
         )[0]
         boxes = result.boxes.xyxy.detach().cpu().numpy()
